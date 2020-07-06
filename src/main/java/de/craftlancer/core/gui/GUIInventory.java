263011e441b667d3658f414f6a4817e3f3c0a7d5
SydMontague <sydmontague@phoenix-staffel.de>
2020-07-06 03:44:42 +0200

    Inventory GUI: Add support for different actions on different ClickTypes

--- src/main/java/de/craftlancer/core/gui/GUIInventory.java
+++ src/main/java/de/craftlancer/core/gui/GUIInventory.java
@@ -1,5 +1,6 @@
 package de.craftlancer.core.gui;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import javax.annotation.Nonnull;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
@@ -15,9 +17,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 public class GUIInventory implements InventoryHolder, Listener {
-
+    
     private final Inventory inventory;
-    private Map<Integer, Runnable> clickActions = new HashMap<>();
+    private Map<Integer, Map<ClickType, Runnable>> clickActions = new HashMap<>();
     
     public GUIInventory(Plugin plugin, String title) {
         this.inventory = Bukkit.createInventory(this, 54, title);
@@ -35,30 +37,35 @@ public class GUIInventory implements InventoryHolder, Listener {
     }
     
     public void fill(ItemStack item) {
-        for(int i = 0; i < inventory.getSize(); i++)
+        for (int i = 0; i < inventory.getSize(); i++)
             inventory.setItem(i, item);
     }
     
     public void setItem(int slotId, ItemStack item) {
-        if(slotId < 0 || slotId >= 54)
+        if (slotId < 0 || slotId >= 54)
             return;
         
         inventory.setItem(slotId, item);
     }
-    
+
     public void setClickAction(int slotId, @Nonnull Runnable action) {
-        if(slotId < 0 || slotId >= 54)
+        setClickAction(slotId, action, null);
+    }
+    
+    public void setClickAction(int slotId, @Nonnull Runnable action, ClickType clickType) {
+        if (slotId < 0 || slotId >= 54)
             return;
         
-        clickActions.put(slotId, action);
+        clickActions.computeIfAbsent(slotId, HashMap::new).put(clickType, action);
     }
     
     @EventHandler
     public void onInteract(InventoryClickEvent event) {
-        if(event.getClickedInventory() != inventory)
+        if (event.getClickedInventory() != inventory)
             return;
         
         event.setCancelled(true);
-        clickActions.getOrDefault(event.getSlot(), () -> {}).run();
+        Map<ClickType, Runnable> map = clickActions.getOrDefault(event.getSlot(), Collections.emptyMap());
+        map.getOrDefault(event.getClick(), map.getOrDefault(null, () -> {})).run();
     }
 }
